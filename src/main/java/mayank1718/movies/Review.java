8f3fbcb961e3b3a047a71929da73f2f91d13cb9e
Mayank Pandey <81166719+codeblack1718@users.noreply.github.com>
2024-02-02 10:41:16 +0100

    Add files via upload

---
+++ src/main/java/mayank1718/movies/Review.java
@@ -0,0 +1,24 @@
+package mayank1718.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.yaml.snakeyaml.events.Event;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+}
---
+++ src/test/java/mayank1718/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package mayank1718.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
